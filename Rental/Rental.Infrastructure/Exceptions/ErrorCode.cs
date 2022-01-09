6c258241d14af2056e00fcf89b7b6c05c3b0edbf
angn92 <gnutekandrzej@gmail.com>
2022-01-09 11:45:14 +0100

    Arrangement error code

--- Rental/Rental.Infrastructure/Exceptions/ErrorCode.cs
+++ Rental/Rental.Infrastructure/Exceptions/ErrorCode.cs
@@ -2,19 +2,37 @@
 {
     public  class ErrorCode
     {
+        public static string IncorrectArgument => "incorrect_argument";
+
+        /// <summary>
+        /// User account error
+        /// </summary>
         public static string UsernameExist => "username_exist";
-        public static string EmailInUse => "email_in_use";
-        public static string InvalidEmail => "invalid_email";
-        public static string InvalidPhoneNumber => "invalid_phone_number";
+        public static string UserNotExist => "user_not_exist";
         public static string AccountBlocked => "account_is_blocked";
         public static string AccountNotActive => "account_is_not_active";
-        public static string ProducyUnavailable => "product_unavailable";
-        public static string InvalidCredential => "invalid_credential";
-        public static string UserNotExist => "user_not_exist";
-        public static string IncorrectArgument => "incorrect_argument";
+
+        /// <summary>
+        /// Email error
+        /// </summary>
+        public static string EmailInUse => "email_in_use";
+        public static string InvalidEmail => "invalid_email";
+
+        /// <summary>
+        /// Phone error 
+        /// </summary>
+        public static string InvalidPhoneNumber => "invalid_phone_number";
+
+        /// <summary>
+        /// Session error
+        /// </summary>
         public static string SessionDoesNotExist => "session_not_exist";
         public static string SessioNotActive => "session_not_active";
         public static string SessionExpired => "session_expired";
+
+        /// <summary>
+        /// Password error 
+        /// </summary>
         public static string PasswordNotExist => "password_not_exist";
     }
 }
