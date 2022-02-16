ca775585e9c72ab345a4c6f6873d23b19a19a3d0
DESKTOP-I1C8DIN\gnute <gnutekandrzej@gmail.com>
2022-02-16 22:59:55 +0100

    Product implementation

---
+++ Rental/Rental.Infrastructure/Handlers/Product/Command/AddProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Rental.Core.Validation;
+using Rental.Infrastructure.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rental.Infrastructure.Handlers.Product.Command
+{
+    public class AddProductCommandHandler : ICommandHandler<AddProductCommand>
+    {
+        public AddProductCommandHandler()
+        {
+
+        }
+
+        public async Task HandleAsync(AddProductCommand command, CancellationToken cancellationToken = default)
+        {
+            ValidationParameter.FailIfNull(command);
+
+
+        }
+    }
+}
