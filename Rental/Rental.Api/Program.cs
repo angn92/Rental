62b1560a104c0ea0375eae762828c7ab52b18ba2
DESKTOP-I1C8DIN\gnute <gnutekandrzej@gmail.com>
2022-03-28 22:38:45 +0200

    Add context db to Program.cs

--- Rental/Rental.Api/Program.cs
+++ Rental/Rental.Api/Program.cs
@@ -1,8 +1,11 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rental.Infrastructure.EF;
 using Rental.Infrastructure.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,10 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(x => x.RegisterModule(new ServiceExtensionModule()));
 
 builder.Services.AddControllers();
+
+//Get connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
