b9e641db6d478a9f88e54d97ea5d382c93a26ac6
DESKTOP-I1C8DIN\gnute <gnutekandrzej@gmail.com>
2022-08-08 21:47:17 +0200

    Booking product handler

---
+++ Rental/Rental.Infrastructure/Handlers/Product/Command/BookingProduct/ProductBookingHandler.cs
@@ -0,0 +1,26 @@
+﻿using Rental.Infrastructure.Command;
+using Rental.Infrastructure.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rental.Infrastructure.Handlers.Product.Command.BookingProduct
+{
+    public class ProductBookingHandler : ICommandHandler<ProductBookingCommand, ProductBookingResponse>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductBookingHandler(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public ValueTask<ProductBookingResponse> HandleAsync(ProductBookingCommand command, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
