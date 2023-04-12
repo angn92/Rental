b348014aae2b71d92f51e03d26f67ea22f64ee53
Andrzej <gnutekandrzej@gmail.com>
2023-04-12 22:26:32 +0200

    Init administrator console application MVC

---
+++ Rental/AdministartionConsole/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AdministartionConsole.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdministartionConsole.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
