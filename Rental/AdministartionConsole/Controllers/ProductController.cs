a70af10154cd7fc4ecb04a072c0ec2291c7ae824
Andrzej <gnutekandrzej@gmail.com>
2023-04-16 21:48:27 +0200

    Create base controller and view for admin console

---
+++ Rental/AdministartionConsole/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdministartionConsole.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
