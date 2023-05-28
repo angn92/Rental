c37aa60c5a55e337b29786de821a7101b20392c4
Andrzej <gnutekandrzej@gmail.com>
2023-05-28 22:44:41 +0200

    Set table row depend on account status

--- Rental/AdministartionConsole/Controllers/CustomerController.cs
+++ Rental/AdministartionConsole/Controllers/CustomerController.cs
@@ -6,19 +6,32 @@ namespace AdministartionConsole.Controllers
     [Route("controller")]
     public class CustomerController : Controller
     {
+        private readonly ILogger<CustomerController> _logger;
         private readonly ICustomerDtoHelper _customerDtoHelper;
 
-        public CustomerController(ICustomerDtoHelper customerDtoHelper)
+        public CustomerController(ILogger<CustomerController> logger, ICustomerDtoHelper customerDtoHelper)
         {
+            _logger = logger;
             _customerDtoHelper = customerDtoHelper;
         }
 
         [HttpGet("customers/list")]
         public async Task<IActionResult> GetCustomer()
         {
-            var customerModel = await _customerDtoHelper.GetCustomerViews();
+            try
+            {
+                var customerModel = await _customerDtoHelper.GetCustomerViews();
+                
+                
 
-            return View(customerModel);
+                return View(customerModel);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
     }
 }
